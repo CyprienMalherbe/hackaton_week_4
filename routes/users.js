6f5649e2fc8e92b6666bb7943fd781cf63ed198e
ShaShaShaaaady <cmalherbe@systra.com>
2022-04-14 14:11:41 +0200

    cyp1411

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var userModel = require('../models/users');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
